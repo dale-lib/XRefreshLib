040f6da8e3be7464817a86becb2193b84b3c5d86
GW00224573 <gw00224573@tech.gwm.cn>
2021-10-13 18:24:45 +0800

    first commit

---
+++ XRefreshLib/src/main/java/com/dale/refresh/listener/OnRefreshListener.java
@@ -0,0 +1,15 @@
+package com.dale.refresh.listener;
+
+
+import androidx.annotation.NonNull;
+
+import com.dale.refresh.api.RefreshLayout;
+
+/**
+ * 刷新监听器
+ * Created by SCWANG on 2017/5/26.
+ */
+
+public interface OnRefreshListener {
+    void onRefresh(@NonNull RefreshLayout refreshLayout);
+}
